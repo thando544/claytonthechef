1eb5c4a83c2ac09be5367a7e1f5458a8cc549e75
thando <mpofu7085@gmail.com>
2025-05-25 14:45:26 +0200

    eyosup

--- main.js
+++ main.js
@@ -313,4 +313,16 @@ document.querySelectorAll('.read-more-btn').forEach(button => {
 });
 
 
-
+/*==============owl carousel===================*/
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+      loop:true,
+      margin:10,
+      nav:true,
+      responsive:{
+        0:{ items:1 },
+        600:{ items:2 },
+        1000:{ items:3 }
+      }
+    });
+  });
